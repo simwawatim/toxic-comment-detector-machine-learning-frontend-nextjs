aee5ad631287c61c530104effe73f6644b90bc3a
tim <simwawatimothy64@gmail.com>
2025-12-13 12:57:58 +0200

    added new

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,30 +1,29 @@
-"use client"
+// app/home/HomeClient.tsx
+"use client";
 
-import { useState } from "react"
-import SidebarComp  from "../components/base/sidenav/sidenav"
-import HeaderComp from "../components/base/header/header"
-import HomePageComp from "../components/home/home"
+import { useState } from "react";
+import SidebarComp from "../components/base/sidenav/sidenav";
+import HeaderComp from "../components/base/header/header";
+import HomePageComp from "../components/home/home";
 
-const HomePage = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    
-    const toggleSidebar = () => setIsOpen(!isOpen)
-    
-    return (
-        <>
-            <div className="flex flex-col h-screen">
-                 
-                    <HeaderComp isOpen={isOpen} toggleSidebar={toggleSidebar} />
-                <div className="flex flex-1 overflow-hidden">
-                       <SidebarComp isOpen={isOpen} toggleSidebar={toggleSidebar} />
-                        
-                    <main className="flex-1 p-4 md:p-6 overflow-y-auto bg-white lg:ml-[250px]">
-                            <HomePageComp />
-                    </main>
-                </div>
-            </div>
-        </>
-    )
-}
+const HomeClient = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default HomePage
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <HeaderComp isOpen={isOpen} toggleSidebar={toggleSidebar} />
+
+      <div className="flex flex-1 overflow-hidden">
+        <SidebarComp isOpen={isOpen} toggleSidebar={toggleSidebar} />
+
+        <main className="flex-1 p-4 md:p-6 overflow-y-auto bg-white lg:ml-[250px]">
+          <HomePageComp />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default HomeClient;
