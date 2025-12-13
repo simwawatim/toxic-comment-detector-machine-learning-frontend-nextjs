7ebe42d9e4d595bdaf026cbee2023c0c547bc4a7
tim <simwawatimothy64@gmail.com>
2025-12-13 12:41:08 +0200

    added new

--- app/api/base/base.ts
+++ app/api/base/base.ts
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "http://127.0.0.1:8000/api/v1/";
-export const BASE_URL = "http://127.0.0.1:8000/";
+export const BASE_API_URL = "https://erp-swagger.izyanehub.com/api/v1/";
+export const BASE_URL = "https://erp-swagger.izyanehub.com/";
 export const DEFAULT_AVATAR = "/default-profile.png";
